053b2b982199523eab477689df3f7a3a5d814e45
danimele <dmgconil@gmail.com>
2022-03-17 12:07:26 +0100

    Propuesta de modificación, 'is' e 'is not' arroja error

--- 4.-contenido_string/main.py
+++ 4.-contenido_string/main.py
@@ -3,10 +3,10 @@ nuevo_mensaje = ''
 pass_caracter = False
 
 for caracter in mensaje:
-    if caracter is ':':
+    if caracter == ':':
         pass_caracter = True
 
-    if pass_caracter == True and caracter is not ':':
+    if pass_caracter == True and caracter == ':':
         nuevo_mensaje = nuevo_mensaje +  caracter
 
 print(nuevo_mensaje)
